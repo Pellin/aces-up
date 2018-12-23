da9cf5d827964e1a17d447c181c3e847e2e23691
Pelle Lindroth <pellelindroth@gmail.com>
2018-12-23 11:36:54 +0100

    changed imageSize.height to 3.2

--- static/script.js
+++ static/script.js
@@ -16,7 +16,7 @@ console.log(innerWidth);
 let x = window.matchMedia('(max-width: 600px)');
 if (x.matches) {
 	imageSize.width = innerWidth / 4.5;
-	imageSize.height = innerWidth / 3;
+	imageSize.height = innerWidth / 3.2;
 	}
 
 container.style.top = '2vh';
